e6e914fbfe8e3d29fcd26cb1fc071298165899d0
gabriel <gacova95@gmail.com>
2019-05-07 21:21:14 -0600

    gui actualizada al 7 de mayo

--- Proyecto2/clienthandler.h
+++ Proyecto2/clienthandler.h
@@ -4,7 +4,8 @@
 
 #include <iostream>
 #include <boost/asio.hpp>
-
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 using namespace boost::asio;
 using ip::tcp;
@@ -17,10 +18,10 @@ private:
     std::string p;
 public:
     std::__cxx11::string getP() const;
-
+    boost::property_tree::ptree propSev;
 public:
     void setP(const string &p);
-    void separarMensaje();
+    void separarMensaje(std::string info);
     void send2Server(std::string dta);
     int a,b;
 };
